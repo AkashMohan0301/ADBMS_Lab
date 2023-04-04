e8236540b730bf98a3ade82b70775b79ec5093ae
Akash Mohan <90025516+AkashMohan0301@users.noreply.github.com>
2023-04-04 14:34:26 +0530

    Add files via upload

--- 07-35/student.sql
+++
@@ -1,8 +0,0 @@
-insert into details values(3,'Ajin','Kuthiravatam',30,20,12,62);
-insert into details values(4,'Hani','Velliparamb',30,20,12,62);
-insert into details values(5,'Sakthi','Seelam',100,100,100,300);
-insert into details values(6,'Fathima','Mutanjeeri',10,10,10,30);
-insert into details values(7,'Akshay','Kakoodi',25,54,78,125);
-insert into details values(8,'Swathi','Koovoor',30,20,12,62);
-insert into details values(9,'Akash MK','Kotooli',58,45,24,178);
-insert into details values(3,'Sreya','Kommeri',4,6,2,12);
